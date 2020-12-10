4e79d66de7b71e7a77d0d5f71bc6265fa54d92af
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-09 19:19:52 -0800

    added function to get proper password length, random gen of chars, put into array

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lwrcase = "abcdefghijklmnopqrstuvwxyz";
+
+var pwd_lngth = 0
+var pwd = [];
+
+
+function writePassword() {
+  pwd_lngth = parseInt(prompt("Please choose a password character length"));
+  while (pwd_lngth < 8 || pwd_lngth > 128) {
+    alert("Password must be no less than 8 characters and no more than 128 characters")
+    pwd_lngth = parseInt(prompt("Pleased choose a password character length within the accepted range"));
+    return pwd_lngth
+  }
+
+  function generatePassword() {
+for (let i = 0; i < pwd_lngth; i++) {
+  pwd.push(lwrcase[Math.floor(Math.random() * 26)]);
+}
+    //gens what will be the password
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
