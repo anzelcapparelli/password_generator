bc6c10da4ed355f1a913f4e1a77921ff6ca358c3
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-10 01:02:23 -0800

    deleting rough draft files, adding comments to increase legibility and comprehension

--- script.js
+++ script.js
@@ -9,23 +9,6 @@ var do_num = false;
 var do_uppr = false;
 var do_special = false;
 
-console.log(special[25]);
-console.log(special.length);
-
-
-// function randLwr() {
-//   var pick = lwrcase[Math.floor(Math.random() * 26)];
-
-// }
-// function randUppr() {
-//   var pick = upprcase[Math.floor(Math.random() * 26)];
-
-// }
-// function special() {
-//   var pick = special[Math.floor(Math.random() * 33)];
-
-// }
-
 
 
 function generatePassword() {
@@ -40,44 +23,39 @@ function generatePassword() {
 
   }
 
-  do_num = confirm("Include numbers?");        // determining if to include numbers in randomization
-  do_uppr = confirm("Include capitalized letters?");        // determining if to include uppercase in randomization
-  do_special = confirm("Include special characters?");        // determining if to include special characters in randomization
+  do_num = confirm("Include numbers?");                        // determining if to include numbers in randomization
+  do_uppr = confirm("Include capitalized letters?");           // determining if to include uppercase in randomization
+  do_special = confirm("Include special characters?");         // determining if to include special characters in randomization
 
-  var pwd = [];
-  var cat_list = [];
+  var pwd = [];           //the empty array that will be catching the randomly generated characters to eventually make the password
+  var cat_list = [];      //the empty list we will be adding each included character refence array to; allows each character to have an equal chance in the selection
   var cat_length = 26;    //min length due to lwrcase
 
 
-  if (do_num) {
+  if (do_num) {                                    //if numbers are allowed, add it to default reference list
     cat_list = lwrcase + num;
-    cat_length = cat_length + num.length
+    cat_length = cat_length + num.length           //add length to default length (allows access via index to each entry in combined list)
   }
 
-  if (do_uppr) {     //lower and uppercase only
+  if (do_uppr) {                                  //if uppercase letters are allowed, add reference to combined reference list
     cat_list = cat_list + upprcase;
-    cat_length = cat_length + upprcase.length;
+    cat_length = cat_length + upprcase.length;    //add length to combined list length (allows access via index to each entry in combined list)
   }
 
-  if (do_special) {     //lower and uppercase only
+  if (do_special) {                               //if uppercase letters are allowed, add reference to combined reference list
     cat_list = cat_list + special;
-    cat_length = cat_length + special.length;
+    cat_length = cat_length + special.length;     //add length to combined list length (allows access via index to each entry in combined list)
   }
 
-  console.log(cat_list);
-  console.log(cat_length);
+
+  for (i = 0; i < pwd_lngth; i++) {                               //random generation and storage!
 
 
-  for (i = 0; i < pwd_lngth; i++) {         //random generation and storage!
-
-
-    pwd.push(cat_list[Math.floor(Math.random() * cat_length)]); //adding to end of pwd array, a rand entry from special
+    pwd.push(cat_list[Math.floor(Math.random() * cat_length)]);   //adding to end of pwd array, a rand entry from combined reference list
   }
 
-  var cat_pwd = pwd.join('');
+  var cat_pwd = pwd.join('');                     //joins individual values in array into single string
   return cat_pwd
-
-
 
 }
 
